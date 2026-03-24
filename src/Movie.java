b1d2343a027e86c3b88889da3aceacbb93b98e17
asjkdhkajsh <skjdhksjdhf@kjsdhf>
2026-03-24 11:03:33 -0400

    Initial commit, need to fix loop in runner

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Movie {
+
+    private String title;
+    private String[] cast;
+    private String director;
+    private String overview;
+    private int runtime;
+    private double userRating;
+
+    public Movie(String title, String cast, String director, String overview, int runtime, double rating){
+        this.title = title;
+        this.cast = cast.split("\\|");
+        this.director = director;
+        this.overview = overview;
+        this.runtime = runtime;
+        this.userRating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String[] getCast() {
+        return cast;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public double getUserRating() {
+        return userRating;
+    }
+
+    @Override
+    public String toString(){
+        return "TITLE: " + title + "\n" +
+                "CAST: " + Arrays.toString(cast) + "\n" +
+                "DIRECTOR: " + director + "\n" +
+                "OVERVIEW: " + overview + "\n" +
+                "RUNTIME: " + runtime + "\n" +
+                "USER RATING: " + userRating;
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Runner {
+    public static void main(String[] args) {
+        File input = new File("src/movies_data.csv");
+        ArrayList<Movie> movieArrayList = new ArrayList<Movie>();
+        try {
+            Scanner scan = new Scanner(input);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        while (){
+
+        }
+    }
 }
